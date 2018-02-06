f90a1039a22355b03cfe1846daf957ea2a34f21f
tannerlp <tannerperdue2011@gmail.com>
2018-02-06 15:29:15 -0700

    lighttpd and wget overloading working

--- extras/dynamicSSA/dynamicSSA.c
+++ extras/dynamicSSA/dynamicSSA.c
@@ -2,8 +2,6 @@
 #define SO_HOSTNAME             85
 #define IPPROTO_TLS             (715 % 255)
 #define SO_PEER_CERTIFICATE     86
-#define SSA_PORT                8443
-#define SSA_ADDR                "127.0.0.1"
 #define MAP_BUCKETS             10
 
 #define LOG_FILE "/tmp/sslLog.txt"
@@ -25,48 +23,25 @@
 typedef struct conn_stat {
     int upgraded;
     int hostname;
-    int ipaddress;
     int keys;
 } conn_stat_t;
 
-typedef int (*orig_socket_f_type)(int domain, int type, int protocol);
-typedef int (*orig_SSL_set_fd_f_type)(SSL *s, int fd);
-typedef SSL *(*orig_SSL_new_f_type)(SSL_CTX *ctx);
 
-int SSA_create_socket();
-int dup_socket_TLS(int fd, const char * ssl_hostname);
-// int SSA_send_fd(int fd);
-ssize_t write_fd(int fd, void* iobuf, size_t nbytes, int sendfd);
-conn_stat_t * print_connection(SSL *s);
-int upgrade_check(SSL *ssl);
-//int dup_socket_TLS(int fd, const char * ssl_hostname);
-int connect_to_host(char* host, char* service);
-X509* PEM_str_to_X509(char* pem_str);
+typedef SSL *(*orig_SSL_new_f_type)(SSL_CTX *ctx);
+typedef void (*orig_SSL_free_f_type)(SSL *ssl);
+
 int logSSL(char* msg);
+int upgrade_check(SSL *ssl);
+X509* PEM_str_to_X509(char* pem_str);
+int dup_socket_TLS(int fd, const char * ssl_hostname);
 
 int upgrade_sock(int fd, SSL *ssl); 
 int SSA_send_fd(int fd, unsigned long id, int is_accepting);
 ssize_t send_fd_to(int fd, void* iobuf, size_t nbytes, int sendfd, struct sockaddr_un* addr, int addr_len);
 
 
-int ssl_map_setup = 0;
-hmap_t* connection_map;
+hmap_t* connection_map = NULL;
 
-
-
-// TCP_ESTABLISHED = 1,
-// TCP_SYN_SENT,
-// TCP_SYN_RECV,
-// TCP_FIN_WAIT1,
-// TCP_FIN_WAIT2,
-// TCP_TIME_WAIT,
-// TCP_CLOSE,
-// TCP_CLOSE_WAIT,
-// TCP_LAST_ACK,
-// TCP_LISTEN,
-// TCP_CLOSING,   /* now a valid state */
-// CP_MAX_STATES /* Leave at the end! */
-//  };
 
 int logSSL(char* msg)
 {
@@ -79,8 +54,7 @@ int logSSL(char* msg)
 SSL *SSL_new(SSL_CTX *ctx)
 {
 
-    if(!ssl_map_setup) {
-        ssl_map_setup = 1;
+    if(!connection_map) {
         connection_map = hashmap_create(MAP_BUCKETS);
     }
 
@@ -95,9 +69,11 @@ SSL *SSL_new(SSL_CTX *ctx)
     conn_stat_t * connection_stat = malloc(conn_stat_len);
     memset(connection_stat,0,conn_stat_len);
     hashmap_add(connection_map, (unsigned long) ssl_obj, connection_stat);
-    // connection_stat->hostname = 0;
-    // connection_stat->ipaddress = 0;
-    // connection_stat->keys = 0;
+
+    // char * message;
+    // asprintf(&message,"New SSL %p\n", ssl_obj);
+    // logSSL(message);
+    // free(message);
 
 
     return ssl_obj;
@@ -106,104 +82,64 @@ SSL *SSL_new(SSL_CTX *ctx)
 
 int SSL_connect(SSL *s)
 {
-    int fd = SSL_get_fd(s);
-    
-
-    conn_stat_t * connection_stat;
-
-    // Not an exisiting SSL object?
-    // connection_stat = print_connection(s);
-
-    // const char * hostname = SSL_get_servername(s,TLSEXT_NAMETYPE_host_name);
-
-    //printf("HOSTNAME %s\n", hostname);
-    
-    // if (setsockopt(fd, IPPROTO_IP, SO_HOSTNAME, hostname, strlen(hostname)+1) == -1){
-    //        perror("setsockopt: SO_HOSTNAME");
-    //        close(fd);
-    // }
-    
-
-    //int new_fd = dup_socket_TLS(old_fd,hostname);
-    //dup2(new_fd,old_fd);
-    //printf("HOSTNAME %s\n", hostname);
-    // logSSL("Called SSL_connect\n");
-    //SSL_set_fd(s,new_fd);
-
-    // Old code for duplication TLS Sockets.
-    // int old_fd = SSL_get_fd(s);
-    // int new_fd = dup_socket_TLS(old_fd,s->tlsext_hostname);
-    // dup2(new_fd,old_fd);
-
+    // Lie that we have connected.
     return 1;
-}
-
-conn_stat_t * print_connection(SSL *s)
-{
-    conn_stat_t * con;
-
-    if (ssl_map_setup) {
-
-        con = (conn_stat_t *) hashmap_get(connection_map,(unsigned long)s);
-        if (con) {
-            // printf("--------------\n"
-            //         "upgraded:%d\nHostname:%d\nipaddress:%d\nkeys:%d\n"
-            //         "--------------\n",con->upgraded,con->hostname,con->ipaddress,con->keys);
-            return con;
-        }
-        else {
-            perror("Connection not in hashmap\n");
-        }
-    }
-    return 0;
 }
 
 int upgrade_check(SSL *ssl)
 {
     int dup_fd;
-    int fd = SSL_get_fd(ssl);
     struct tcp_info info;
-    int infoLen = sizeof(info);
     const char * hostname = NULL;
-
     char * message = NULL; 
+    conn_stat_t * con = NULL;
+    int fd = SSL_get_fd(ssl);
+    int infoLen = sizeof(info);
 
-    conn_stat_t * con = print_connection(ssl);
-    if ( !con->upgraded ) {
+    if (connection_map) {
 
-        memset(&info,0,infoLen);
-        getsockopt(fd, SOL_TCP, TCP_INFO, (void *)&info, (socklen_t *)&infoLen);
-
-        if ( con->hostname ) {
-            hostname = SSL_get_servername(ssl,TLSEXT_NAMETYPE_host_name);
+        con = (conn_stat_t *) hashmap_get(connection_map,(unsigned long)ssl);
+        if (!con) {
+            perror("Connection not in hashmap\n");
         }
 
-        // What states do we want to upgrade and which ones do we want to leave alone?
-        if (info.tcpi_state == TCP_ESTABLISHED) {
-            upgrade_sock(SSL_get_fd(ssl),ssl);
-            /* code */
-            if (hostname)
-            {
-                if (setsockopt(fd, IPPROTO_IP, SO_HOSTNAME, hostname, strlen(hostname)+1) == -1){
-                   perror("setsockopt: SO_HOSTNAME");
-                   close(fd);
+        if ( !con->upgraded ) {
+            memset(&info,0,infoLen);
+            getsockopt(fd, SOL_TCP, TCP_INFO, (void *)&info, (socklen_t *)&infoLen);
+
+            if ( con->hostname ) {
+                hostname = SSL_get_servername(ssl,TLSEXT_NAMETYPE_host_name);
+            }
+
+            // What states do we want to upgrade and which ones do we want to leave alone?
+            if (info.tcpi_state == TCP_ESTABLISHED) {
+                upgrade_sock(SSL_get_fd(ssl),ssl);
+                /* code */
+                if (hostname)
+                {
+                    if (setsockopt(fd, IPPROTO_IP, SO_HOSTNAME, hostname, strlen(hostname)+1) == -1){
+                       perror("setsockopt: SO_HOSTNAME");
+                       close(fd);
+                    }
                 }
             }
+
+            //Not connected duplicate connection
+            else if(info.tcpi_state == 0)
+            {
+                dup_fd = dup_socket_TLS(fd,hostname);
+                dup2(dup_fd,fd);
+            }
+            else
+            {
+                perror("Unaccounted connection state");
+                // asprintf(&message,"Unaccounted state? %u\n", info.tcpi_state);
+                // logSSL(message);
+                // free(message);
+            }
+            
+            con->upgraded = 1;
         }
-        //Not connected duplicate connection
-        else if(info.tcpi_state == 0)
-        {
-            dup_fd = dup_socket_TLS(fd,hostname);
-            dup2(dup_fd,fd);
-        }
-        else
-        {
-            asprintf(&message,"Unaccounted state? %u\n", info.tcpi_state);
-            logSSL(message);
-            free(message);
-        }
-        
-        con->upgraded = 1;
     }
 }
 
@@ -212,11 +148,8 @@ int SSL_write(SSL *ssl, const void *buf, int num)
     int wfd, ret;
 
     upgrade_check(ssl);
-
-    logSSL("Writing to socket\n");
-
+    // logSSL("Writing to socket\n");
     wfd = SSL_get_wfd(ssl);
-
     return send(wfd, buf, num, 0);
 
 }
@@ -226,11 +159,8 @@ int SSL_read(SSL *ssl, void *buf, int num)
     int rfd, ret;
 
     upgrade_check(ssl);
-
-    logSSL("Reading from socket\n");
-
+    // logSSL("Reading from socket\n");
     rfd = SSL_get_rfd(ssl);
-
     return recv(rfd, buf, num, 0);
 
 }
@@ -240,31 +170,29 @@ int SSL_peek(SSL *ssl, void *buf, int num)
     int rfd, ret;
 
     upgrade_check(ssl);
-
-    logSSL("Peek from socket\n");
-
+    // logSSL("Peek from socket\n");
     rfd = SSL_get_rfd(ssl);
-
     return recv(rfd, buf, num, MSG_PEEK);
 
 }
 
+// Lie that we have finished
 int SSL_is_init_finished(SSL *s)
 {
     return 1;
 }
 
+// SSL_ERROR_WANT_READ Lie to litt
 int SSL_get_error(const SSL *ssl, int ret)
 {
-
-    return 2;
+    // Lie for error
+    return SSL_ERROR_WANT_READ;
 }
 
 
 X509 *SSL_get_peer_certificate(const SSL *s)
 {
     int ssl_fd = SSL_get_rfd(s);
-
     int cert_len = 1024*4;
     char cert[1024*4];
 
@@ -330,23 +258,25 @@ int upgrade_sock(int fd, SSL *ssl) {
 
     is_accepting = SSL_in_accept_init(ssl) ? 1 : 0;
 
-    file_flags = fcntl(fd, F_GETFL, 0);
+    file_flags = fcntl(new_fd, F_GETFL, 0);
     if (file_flags == -1) {
+        // logSSL("fcntl get");
         perror("fcntl get flags");
     }
     
     file_flags |= O_NONBLOCK;
-    if (fcntl(fd, F_SETFL, file_flags) == -1)
+    if (fcntl(new_fd, F_SETFL, file_flags) == -1)
     {
+        // logSSL("fcntl set");
         perror("fcntl set non blocking");
     }
 
     SSA_send_fd(fd, id, is_accepting);
     if(connect(new_fd, (struct sockaddr*)&addr, sizeof(addr)) == -1) {
-        perror("connect SSA FD");
+        // perror("connect SSA FD");
     }
     dup2(new_fd, fd);
-    logSSL("Upgraded new socket\n");
+    // logSSL("Upgraded new socket\n");
     return 0;
 }
 
@@ -468,24 +398,21 @@ int dup_socket_TLS(int sock_fd, const char * ssl_hostname)
 
 }
 
+void SSL_free(SSL *ssl)
+{
+    char * message;
 
-// int SSL_set_fd (SSL *s, int fd)
-// {
+    orig_SSL_free_f_type orig_SSL_free;
+    orig_SSL_free = (orig_SSL_free_f_type)dlsym(RTLD_NEXT,"SSL_free");
 
+    conn_stat_t * con;
+    con = (conn_stat_t *) hashmap_get(connection_map,(unsigned long)ssl);
+    free(con);
+    int del_ret = hashmap_del(connection_map, (unsigned long)ssl);
 
-//     // struct tcp_info info;
-//     // int infoLen = sizeof(info);
-//     // getsockopt(fd, SOL_TCP, TCP_INFO, (void *)&info, (socklen_t *)&infoLen);
-//     // fprintf(stderr,"\nSET_FD:%u\n", info.tcpi_state); 
-
-//     //SSA_send_fd(fd);
-
-//     // int ssa_fd = SSA_create_socket();
-
-//     // dup2(ssa_fd,fd);
-
-//     orig_SSL_set_fd_f_type orig_SSL_set_fd;
-//     orig_SSL_set_fd = (orig_SSL_set_fd_f_type)dlsym(RTLD_NEXT,"SSL_set_fd");
-//     return orig_SSL_set_fd(s,fd);
-//     // orig_SSL_set_fd_f_type
-// }
+    
+    // asprintf(&message,"SSL_FREE %d sslObj %p\n", del_ret,ssl);
+    // logSSL(message);
+    // free(message);
+    orig_SSL_free(ssl);
+}
