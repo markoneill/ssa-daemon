5c3d900c88d02f7af566987a453e4b30f816eff4
Torstein Collett <t3ttelloc@gmail.com>
2018-06-05 11:56:49 -0600

    added a script to install all necessary libraries for fedora

---
+++ install_packages.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+echo 'Installing libraries for tls_wrapper'
+sudo dnf install kernel-devel libnl3-devel libevent-devel openssl-devel avahi-devel libconfig-devel gtk3-devel elfutils-libelf-devel
+echo 'Installed'
