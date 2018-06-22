86f36c7b69d67469ed15fd60f9e7fc9501c7ecfe
tannerlp <tannerperdue2011@gmail.com>
2018-06-22 14:20:42 -0600

    parse substring with welcome message button says sign in to checkout if not logged in

--- test_files/webserver-event/www4/account/index.php
+++ test_files/webserver-event/www4/account/index.php
@@ -3,9 +3,39 @@ session_start();
 require('../items.php');
 require('../header.php');
 
-$name = "No one";
+$cert_string = "No one";
 if (isset($_SERVER['SSA_ID'])) {
-	$name = $_SERVER['SSA_ID'];
+	$cert_string = $_SERVER['SSA_ID'];
+}else{
+    echo "you need to be logged in, n00b~!";
+    exit();
+}
+
+
+$string_arr = explode('/',$cert_string);
+
+$_SERVER['name'] = NULL;
+$_SERVER['location'] = NULL;
+$_SERVER['phone'] = NULL;
+$_SERVER['email'] = NULL;
+
+for ($i = 0; $i < sizeof($string_arr); $i++){
+    $abbr = explode('=',$string_arr[$i];
+    switch ($abbr[0]){
+        case "C":
+        $_SERVER['location'] = $abbr[1];
+        break;
+        case "CN":
+        $_SERVER['name'] = $abbr[1];
+        break;
+        case "O":
+        $_SERVER['phone'] = $abbr[1];
+        break;
+        case "emailAddress":
+        $_SERVER['email'] = $abbr[1];
+        break;
+
+    }
 }
 
 ?>
@@ -13,7 +43,17 @@ if (isset($_SERVER['SSA_ID'])) {
   <div class="row">
     <div class="col-sm-12">
       <div class="panel panel-primary">
-	<?php echo 'Welcome, ', $name; ?>
+	<?php
+    if (!isset($_SERVER['name'])){
+        echo "somehow, name wasn't set... ";    
+    }else{
+        echo 'Welcome, ', $_SERVER['name'];    
+        if(isset($_POST['checkout']){
+            //TODO does this work??? do I need to reset the post value or something> I don't think so
+            // set button proceed to checkout
+        }
+    }
+    ?>
       </div>
     </div>
   </div>
