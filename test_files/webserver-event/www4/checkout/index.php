c8f038c6563ebd15f18c5c7eec502813e0e7c2a6
masoncoram <mcoram777@yahoo.com>
2018-07-05 12:14:11 -0600

    Added jquery to script file i guess

--- test_files/webserver-event/www4/checkout/index.php
+++ test_files/webserver-event/www4/checkout/index.php
@@ -176,7 +176,7 @@ function checkout() {
 		</div>
 	</div>
 </div><br /><br />
-<script>
+<script type="text/javascript" src="http://code.jquery.com/jquery-1.7.1.min.js">
 $('#firstName, #lastName, #address, #address2, #country, #state, #city, #zip, #firstNameBilling, #lastNameBilling, #addressBilling, #address2Billing, #countryBilling, #stateBilling, #cityBilling, #zipBilling, #cc-name, #cc-number, #cc-expiration, #cc-cvv').bind('keyup', function() {
 	if(allFilled()) {
 		$('#purchase').removeAttr('disabled');
