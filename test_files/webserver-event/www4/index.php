d89d7a6f5d311266885317bd44bc99ad646dbc28
masoncoram <mcoram777@yahoo.com>
2018-07-10 12:56:52 -0600

    Still testing out checkout popup

--- test_files/webserver-event/www4/index.php
+++ test_files/webserver-event/www4/index.php
@@ -1,9 +1,158 @@
-<?php 
+<?php
 session_start();
 ini_set('display_errors', 'On');
 error_reporting(E_ALL);
 require('items.php');
 require('header.php');
+
+?>
+
+
+
+
+<!DOCTYPE html>
+<html>
+<title>Modal Popup Box</title>
+<style>
+*{margin:0px; padding:0px; font-family:Helvetica, Arial, sans-serif;}
+
+/* Full-width input fields */
+input[type=text], input[type=password] {
+    width: 90%;
+    padding: 12px 20px;
+    margin: 8px 26px;
+    display: inline-block;
+    border: 1px solid #ccc;
+    box-sizing: border-box;
+        font-size:16px;
+}
+
+/* Set a style for all buttons*/
+#checkout-button {
+    background-color: #4CAF50;
+    color: white;
+    padding: 14px 20px;
+    margin: 8px 26px;
+    border: none;
+    cursor: pointer;
+    width: 90%;
+        font-size:20px;
+}
+#checkout-button:hover {
+    opacity: 0.8;
+}
+
+/* Center the image and position the close button */
+.imgcontainer {
+    text-align: center;
+    margin: 24px 0 12px 0;
+    position: relative;
+}
+.avatar {
+    width: 200px;
+        height:200px;
+    border-radius: 50%;
+}
+
+/* The Modal (background) */
+.modal {
+        display:none;
+    position: fixed;
+    z-index: 1;
+    left: 0;
+    top: 0;
+    width: 100%;
+    height: 100%;
+    overflow: auto;
+    background-color: rgba(0,0,0,0.4);
+}
+
+/* Modal Content Box */
+.modal-content {
+    background-color: #fefefe;
+    margin: 4% auto 15% auto;
+    border: 1px solid #888;
+    width: 40%;
+    padding-left: 15px;
+    padding-right:15px;
+        padding-bottom: 30px;
+}
+
+/* The Close Button (x) */
+.close {
+    position: absolute;
+    right: 25px;
+    top: 0;
+    color: #000;
+    font-size: 35px;
+    font-weight: bold;
+}
+.close:hover,.close:focus {
+    color: red;
+    cursor: pointer;
+}
+
+/* Add Zoom Animation */
+.animate {
+    animation: zoom 0.6s
+}
+@keyframes zoom {
+    from {transform: scale(0)}
+    to {transform: scale(1)}
+}
+</style>
+<body background="../background1.png">
+
+<h1 style="text-align:center; font-size:50px; color:#fff">Modal Popup Box Login Form</h1>
+
+<button onclick="document.getElementById('modal-wrapper').style.display='block'" id='checkout-button' style="width:200px; margin-top:200px; margin-left:160px;">
+Open Popup</button>
+
+<div id="modal-wrapper" class="modal">
+
+  <form class="modal-content animate" action="/cart/">
+
+    <div class="imgcontainer">
+      <span onclick="document.getElementById('modal-wrapper').style.display='none'" class="close" title="Close PopUp">&times;</span>
+      <img src="cart2.png" alt="Avatar" class="avatar">
+      <h1 style="text-align:center">Current Cart</h1>
+    </div>
+
+    <?php showCart(); ?>
+
+    <div class="container">
+      <button type="submit" class="btn btn-success">Checkout</button>
+      <button onclick="document.getElementById('modal-wrapper').style.display='none'" type="button" class="btn btn-success">Keep Shopping</button>
+    </div>
+
+  </form>
+
+</div>
+
+<script>
+// If user clicks anywhere outside of the modal, Modal will close
+
+var modal = document.getElementById('modal-wrapper');
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
+</script>
+
+</body>
+</html>
+
+
+
+<?php 
+/*
+session_start();
+ini_set('display_errors', 'On');
+error_reporting(E_ALL);
+require('items.php');
+require('header.php');
+ */
 
 function showItems($category) {
 	global $items;
@@ -54,6 +203,73 @@ function showItems($category) {
 	}
 	echo '  </div>';
 	echo '</div><br />';
+}
+
+function showCart() {
+        setlocale(LC_MONETARY, 'en_US.UTF-8');
+        global $items;
+        $categories = array_keys($items);
+        echo '<table class="table table-striped" width="100%">';
+        echo '  <thead>';
+        echo '    <tr>';
+        echo '      <th>Item</th>';
+        echo '      <th>Unit Price</th>';
+        echo '      <th>Quantity</th>';
+        echo '      <th>Subtotal</th>';
+        echo '    </tr>';
+        echo '  </thead>';
+        echo '  <tbody>';
+        $total = 0;
+        foreach ($categories as $category) {
+                $i = 0;
+                foreach ($items[$category] as $item) {
+                        $quantity = 0;
+                        if (isset($_SESSION[$item['Name']])) {
+                                $quantity = $_SESSION[$item['Name']];
+                                $subtotal = floatval($item['Price']) * floatval($quantity);
+                                $total += $subtotal;
+                                echo '<tr>';
+                                echo '  <td>', $item['Name'], '</td>';
+				echo '  <td>', money_format('%.2n', $item['Price']), '</td>';
+				echo '  <td>', $quantity, '</td>';
+				/*
+                                echo '  <td>';
+                                echo '          <form class="form-inline" method="post" action="/cart/">';
+                                echo '          <input type="text" name="q" size="2" class="form-control" value="', $quantity ,'" />';
+                                echo '          <input type="hidden" name="p" value="', $i ,'" />';
+                                echo '          <input type="hidden" name="s" value="', $category ,'" />';
+                                echo '          <input type="hidden" name="update" value="1" />';
+                                echo '          </form>';
+				echo '  </td>';
+				 */
+				echo '  <td>', money_format('%.2n', $subtotal), '</td>';
+				/*
+                                echo '  <td>';
+                                echo '          <form class="form-inline" method="post" action="/cart/">';
+                                echo '          <input type="hidden" name="p" value="', $i ,'" />';
+                                echo '          <input type="hidden" name="s" value="', $category ,'" />';
+                                echo '          <input type="hidden" name="del" value="1" />';
+                                echo '          </form>';
+				echo '  </td>';
+				 */
+                                echo '</tr>';
+                        }
+                        $i++;
+                }
+        }
+        echo '  </tbody>';
+        echo '  <tfoot>';
+        $totalStr = money_format('%.2n', $total);
+
+        echo '    <tr>';
+        echo '      <td colspan="4" class="total">Total: ', $totalStr, '</td>';
+        /*
+	echo '      <td>';
+	echo '      </td>';
+	 */
+        echo '    </tr>';
+        echo '  </tfoot>';
+        echo '</table>';
 }
 
 ?>
