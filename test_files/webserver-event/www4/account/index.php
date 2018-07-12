2469bbdbe7c679332f7c959ed95e53dccc52e631
Torstein Collett <t3ttelloc@gmail.com>
2018-07-12 10:47:47 -0600

    added a logout button on the website to clear the variables

--- test_files/webserver-event/www4/account/index.php
+++ test_files/webserver-event/www4/account/index.php
@@ -1,8 +1,12 @@
 <?php 
 session_start();
+if (isset($_POST["logout"])) {
+	session_unset();
+	unset($_SERVER['SSA_ID']);
+}
+require('../items.php');
+require('../header.php');
 if (isset($_SERVER['SSA_ID'])) {
-	require('../items.php');
-	require('../header.php');
 ?>
 <div class="container"> 
   <div class="row">
@@ -10,11 +14,14 @@ if (isset($_SERVER['SSA_ID'])) {
       <div class="panel">
 	<h1>Welcome, <?php echo $_SESSION['name']; ?></h1>
 	<p>You are logged in securely!</p>
+	<form class="form-inline" method="post" action="/account/">
+		<div class="btn-group btn-grou-lg">
+			<button type="submit" class="btn btn-primary" name='logout' value="true">Logout</button>
+		</div>
+	</form>
 <?php
 }
 else {
-	require('../items.php');
-	require('../header.php');
 ?>
 <div class="container">
   <div class="row">
@@ -24,9 +31,9 @@ else {
 	<p>That means you can securely sign in with keys stored on your phone!</p>
 	<p>Please click the button below to register an account with your phone</p>
 	<form class="form-inline" method="post" action="/login/">
-	<div class="btn-group btn-grou-lg">
-	  <button type="submit" class="btn btn-primary">Register</button>
-	</div>
+		<div class="btn-group btn-grou-lg">
+			<button type="submit" class="btn btn-primary">Register</button>
+		</div>
         </form>
 <?php
 }
