d259e31f47fab7664335d7f274aa8855723fd835
benjamin <benjaminhdavis8@gmail.com>
2018-08-09 15:41:24 -0600

    changed SO_PEAR_CERTIFICATE to to TLS_PEAR_CERTIFICATE_CHAIN

--- in_tls.h
+++ in_tls.h
@@ -5,20 +5,20 @@
 #define IPPROTO_TLS 	(715 % 255)
 
 /* Options */
-#define TCP_REMOTE_HOSTNAME               85
-#define TCP_HOSTNAME                      86
-#define TCP_TRUSTED_PEER_CERTIFICATES     87
-#define TCP_CERTIFICATE_CHAIN             88
-#define TCP_PRIVATE_KEY                   89
-#define TCP_ALPN                          90
-#define TCP_SESSION_TTL                   91
-#define TCP_DISABLE_CIPHER                92
+#define TCP_REMOTE_HOSTNAME              85
+#define TCP_HOSTNAME                     86
+#define TCP_TRUSTED_PEER_CERTIFICATES    87
+#define TCP_CERTIFICATE_CHAIN            88
+#define TCP_PRIVATE_KEY                  89
+#define TCP_ALPN                         90
+#define TCP_SESSION_TTL                  91
+#define TCP_DISABLE_CIPHER               92
 #define TCP_PEER_IDENTITY		 93
 #define TCP_REQUEST_PEER_AUTH		 94
 
 /* Internal use only */
-#define TCP_PEER_CERTIFICATE              95
-#define TCP_ID                            96
+#define TCP_PEER_CERTIFICATE_CHAIN       95
+#define TCP_ID                           96
 
 /* TCP options */
 #define TCP_UPGRADE_TLS         33
