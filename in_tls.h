7c3c5d4ecf49fa1b7a5fb18fc2b2e779a5f4d8ee
benjamin <benjaminhdavis8@gmail.com>
2018-08-10 08:25:33 -0600

    switched TCP to TLS

--- in_tls.h
+++ in_tls.h
@@ -2,39 +2,40 @@
 #define IN_TLS
 
 /* Protocol */
-#define IPPROTO_TLS 	(715 % 255)
+#define IPPROTO_TLS     (715 % 255)
 
 /* Options */
-#define TCP_REMOTE_HOSTNAME              85
-#define TCP_HOSTNAME                     86
-#define TCP_TRUSTED_PEER_CERTIFICATES    87
-#define TCP_CERTIFICATE_CHAIN            88
-#define TCP_PRIVATE_KEY                  89
-#define TCP_ALPN                         90
-#define TCP_SESSION_TTL                  91
-#define TCP_DISABLE_CIPHER               92
-#define TCP_PEER_IDENTITY		 93
-#define TCP_REQUEST_PEER_AUTH		 94
+#define TLS_REMOTE_HOSTNAME               85
+#define TLS_HOSTNAME                      86
+#define TLS_TRUSTED_PEER_CERTIFICATES     87
+#define TLS_CERTIFICATE_CHAIN             88
+#define TLS_PRIVATE_KEY                   89
+#define TLS_ALPN                          90
+#define TLS_SESSION_TTL                   91
+#define TLS_DISABLE_CIPHER                92
+#define TLS_PEER_IDENTITY		  93
+#define TLS_REQUEST_PEER_AUTH		  94
 
 /* Internal use only */
-#define TCP_PEER_CERTIFICATE_CHAIN       95
-#define TCP_ID                           96
+#define TLS_PEER_CERTIFICATE_CHAIN        95
+#define TLS_ID                            96
 
 /* TCP options */
 #define TCP_UPGRADE_TLS         33
 
 /* Address types */
-#define AF_HOSTNAME	43
+#define AF_HOSTNAME     43
 
-struct host_addr { 
-	unsigned char name[255]; 
-}; 
- 
-struct sockaddr_host { 
-	sa_family_t sin_family; 
-	unsigned short sin_port; 
-	struct host_addr sin_addr; 
-}; 
+struct host_addr {
+        unsigned char name[255];
+};
+
+struct sockaddr_host {
+        sa_family_t sin_family;
+        unsigned short sin_port;
+        struct host_addr sin_addr;
+};
 
 
 #endif
+
